8c95a02608180d756eb8ac50217b76ece0322913
teachwiki <35622257+teachwiki@users.noreply.github.com>
2021-01-19 07:02:37 -0800

    Update service-worker.min.js

--- service-worker.min.js
+++ service-worker.min.js
@@ -7,8 +7,8 @@
 var ICONFIG = {
     tmpVersion: 1570891429565,
     dataCacheName: "data-v3",
-    cacheName: "sketchpad.pro",
-    domain: "sketchpad.pro",
+    cacheName: "teachwiki.github.io/sketch.io",
+    domain: "teachwiki.github.io/sketch.io",
     shareTitle: "Invitation to Sketchpad.pro",
     shareText: "Join to real-time collaboration whiteboard",
     mailtoSubject: "Invitation to Sketchpad.pro",
